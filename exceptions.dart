0d7b0564dfdf1881ae4d6e0577e9b4bf48a1c20c
doniyor17 <58927624+doniyor17@users.noreply.github.com>
2021-03-04 13:02:50 +0500

    end exception handling block
    Exception handling, creating custom exception class!

--- exceptions.dart
+++ exceptions.dart
@@ -1,35 +1,52 @@
 void main() {
   // CASE When you know exception name
-  try {
-    int result = 12 ~/ 0;
-    print(result);
-  } on IntegerDivisionByZeroException {
-    print("Devide an integer is not logic");
-  }
+  // try {
+  //   int result = 12 ~/ 0;
+  //   print(result);
+  // } on IntegerDivisionByZeroException {
+  //   print("Devide an integer is not logic");
+  // }
 
-  // CASE When you don't know exception name
-  try {
-    int result = 12 ~/ 0;
-    print(result);
-  } catch (e) {
-    print("Exception is $e!");
-  }
+  // // CASE When you don't know exception name
+  // try {
+  //   int result = 12 ~/ 0;
+  //   print(result);
+  // } catch (e) {
+  //   print("Exception is $e!");
+  // }
 
-  // CASE STACK TRACE before exception what happened
-  try {
-    int result = 12 ~/ 0;
-    print(result);
-  } catch (e, s) {
-    print("STACk TRACE is $s");
-  }
+  // // CASE STACK TRACE before exception what happened
+  // try {
+  //   int result = 12 ~/ 0;
+  //   print(result);
+  // } catch (e, s) {
+  //   print("STACk TRACE is $s");
+  // }
 
   // CASE FINALLY statement execute whether there is exception or not
+  // try {
+  //   int result = 12 ~/ 4;
+  //   print(result);
+  // } catch (e) {
+  //   print("Exception is $e!");
+  // } finally {
+  //   print("This is finally statement!");
+  // }
   try {
-    int result = 12 ~/ 4;
-    print(result);
+    depositMoney(-500);
   } catch (e) {
-    print("Exception is $e!");
-  } finally {
-    print("This is finally statement!");
+    print(e.errorMessage());
+  }
+}
+
+class DepositException implements Exception {
+  String errorMessage() {
+    return 'You cannot insert less than Zero';
+  }
+}
+
+void depositMoney(int amount) {
+  if (amount < 0) {
+    throw new DepositException();
   }
 }
