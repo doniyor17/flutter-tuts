d8584835837491641ce5250c0684c2d3144fd5ce
doniyor17 <58927624+doniyor17@users.noreply.github.com>
2021-03-04 11:34:19 +0500

    handle exceptions basic

---
+++ exceptions.dart
@@ -0,0 +1,35 @@
+void main() {
+  // CASE When you know exception name
+  try {
+    int result = 12 ~/ 0;
+    print(result);
+  } on IntegerDivisionByZeroException {
+    print("Devide an integer is not logic");
+  }
+
+  // CASE When you don't know exception name
+  try {
+    int result = 12 ~/ 0;
+    print(result);
+  } catch (e) {
+    print("Exception is $e!");
+  }
+
+  // CASE STACK TRACE before exception what happened
+  try {
+    int result = 12 ~/ 0;
+    print(result);
+  } catch (e, s) {
+    print("STACk TRACE is $s");
+  }
+
+  // CASE FINALLY statement execute whether there is exception or not
+  try {
+    int result = 12 ~/ 4;
+    print(result);
+  } catch (e) {
+    print("Exception is $e!");
+  } finally {
+    print("This is finally statement!");
+  }
+}
