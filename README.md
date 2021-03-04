c646f5161ee83df9a2b087faae149254ae36f506
doniyor17 <58927624+doniyor17@users.noreply.github.com>
2021-03-04 10:08:33 +0500

    learn loop in dart
    Loops (for, while, do... while) break and continue statements

--- README.md
+++
@@ -1,2 +0,0 @@
-# flutter-tuts
- Beginning Flutter
---
+++ for.dart
@@ -0,0 +1,10 @@
+void main() {
+  for (var i = 1; i <= 3; i++) {
+    for (var j = 1; j <= 3; j++) {
+      if (i == 2 && j == 2) {
+        continue;
+      }
+      print("$i $j");
+    }
+  }
+}
