3f200484b05ba2db53a37d5ff9c01cf29e3779a4
doniyor17 <58927624+doniyor17@users.noreply.github.com>
2021-03-05 11:50:43 +0500

    add method and property overriding
    Simple example of overriding in dart!

--- inheritance.dart
+++ inheritance.dart
@@ -1,45 +1,27 @@
 void main() {
-  var dog = Dog();
-  dog.breed = 'salom';
-  dog.color = 'White';
-  dog.bark();
-  dog.eat();
-
-  print('~~~~~~~~~~~~~~~~~');
-
   var cat = Cat();
-  cat.age = 7;
-  cat.color = 'Brown';
   cat.meow();
   cat.eat();
-  cat.sleep();
-
+  print(cat.color);
   print('~~~~~~~~~~~~~~~~~');
 }
 
 class Animals {
-  String color;
+  String color = "Black";
   void eat() {
     print("I'm eating!");
   }
 }
 
-class Dog extends Animals {
-  String breed;
-  void bark() {
-    print('Woof!');
-  }
-}
-
 class Cat extends Animals {
   int age;
-  String _sl;
-  void sleep() {
-    _sl = "I'm sleeping!";
-    print(_sl);
-  }
-
+  String color = "Brown"; // |> PROPERTY OVERRIDING
   void meow() {
     print('Meow');
+  }
+
+  void eat() {
+    print('Meow I am eating!'); // |> METHOD OVERRIDING
+    super.eat();
   }
 }
